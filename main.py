c41a551ca8edcd1fa2d0cce23bcb8191d4dc8e34
Miendy <ultrapheonix82@gmail.com>
2022-03-12 17:21:13 +1030

    4

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+import math
+
+root = Tk()
+root.title("Random Texturepack Generator")
+root.geometry("600x600")
+root.resizable(False, False)
+root.config(bg='#2F3136')
+
+root.mainloop()
