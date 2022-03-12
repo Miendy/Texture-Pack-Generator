b0d43fef2cb60624985bb685a3390c13f024be3c
Miendy <ultrapheonix82@gmail.com>
2022-03-12 18:08:24 +1030

    update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from tkinter import *
 import math
 
 root = Tk()
-root.title("Random Texturepack Generator")
 root.geometry("700x600")
 root.resizable(False, False)
 root.config(bg='#2F3136')
@@ -12,12 +11,24 @@ root.overrideredirect(True)
 def move(event):
     root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
 
-title_bar = Frame(root, bg = '#2F3136', relief = 'raised', bd = 2)
-title_bar.pack(side=LEFT)
+def minimize1():
+    root.state(newstate='iconic')
 
-title = Label(title_bar, text = 'x', bg = '#2F3136', fg = "red")
-title.pack(side=LEFT)
+def maximize():
+    root.state(newstate='normal')
+
+title_bar = Frame(root, bg = '#2F3136', relief = 'raised')
+title_bar.pack(side=TOP, fill=BOTH)
+
+title = Label(title_bar, text = 'Random Texturepack Generator', bg = '#2F3136', fg = "#F0F0F0")
+title.pack(side=RIGHT)
 
 title_bar.bind('<B1-Motion>', move)
+
+close_button = Button(title_bar, text='  x  ', font = 'Fredoka-Bold', command = root.destroy, bg = '#2F3136', fg = "red")
+close_button.pack(side=LEFT)
+
+min_button = Button(title_bar, text='  -  ', font = 'Fredoka-Bold', command = minimize1, bg = '#2F3136', fg = "red")
+min_button.pack(side=LEFT)
 
 root.mainloop()
