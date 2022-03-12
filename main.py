a9501c033408102149e417cb5f1a98f61b2bdaec
Miendy <ultrapheonix82@gmail.com>
2022-03-12 17:30:02 +1030

    something

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tkinter as tk
 from tkinter import *
 import math
@@ -10,13 +9,3 @@ root.resizable(False, False)
 root.config(bg='#2F3136')
 
 root.mainloop()
-=======
-import tkinter as tk
-from tkinter import *
-import math
-
-root = Tk()
-root.title("Random Texturepack Generator")
-root.geometry("360x600")
-root.resizable(False, False)
->>>>>>> 052a101d1b683f6fe6e2dc237a129bc135935bc1
