bf36992ab0e7e65d44b0e35f679a8468d07dfa57
Miendy <ultrapheonix82@gmail.com>
2022-03-12 17:50:30 +1030

    update

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ root.title("Random Texturepack Generator")
 root.geometry("700x600")
 root.resizable(False, False)
 root.config(bg='#2F3136')
+root.overrideredirect(True)
+
+def move(event):
+    root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
+
+title_bar = Frame(root, bg = '#2F3136', relief = 'raised', bd = 2)
+title_bar.pack(side=LEFT)
+
+title = Label(title_bar, text = 'x', bg = '#2F3136', fg = "red")
+title.pack(side=LEFT)
+
+title_bar.bind('<B1-Motion>', move)
 
 root.mainloop()
