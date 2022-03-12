d8b7a17e1fd4e130c7a63ed18a5ce51c01bd1012
Miendy <ultrapheonix82@gmail.com>
2022-03-12 18:48:37 +1030

    12123

--- main.py
+++ main.py
@@ -7,28 +7,25 @@ root.geometry("700x600")
 root.resizable(True, True)
 root.config(bg='#2F3136')
 root.overrideredirect(True)
+root.eval('tk::PlaceWindow . center')
 
 root.minimized = False
 root.maximized = False
 
 def move(event):
-    root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
-
-def minimize():
-    root.attributes("-alpha",0) # so you can't see the window when is minimized
-    root.minimized = True 
+    root.geometry('+{0}+{1}'.format(event.x_root - 350, event.y_root - 10))
 
 def maximize():
 
     if root.maximized == False: # if the window was not maximized
         root.normal_size = root.geometry()
-        expand_button.config(text=" 🗗 ")
+        expand_button.config(text="   🗗   ")
         root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0")
         root.maximized = not root.maximized 
         # now it's maximized
         
     else: # if the window was maximized
-        expand_button.config(text=" 🗖 ")
+        expand_button.config(text="   🗖   ")
         root.geometry(root.normal_size)
         root.maximized = not root.maximized
         # now it is not maximized
@@ -41,13 +38,10 @@ title.pack(side=RIGHT)
 
 title_bar.bind('<B1-Motion>', move)
 
-close_button = Button(title_bar, text='  ✖  ', font = 'Fredoka-Bold', command = root.destroy, bg = '#2F3136', fg = "red")
+close_button = Button(title_bar, text='  ❌  ', command = root.destroy, bg = '#2F3136', fg = "red")
 close_button.pack(side=LEFT)
 
-min_button = Button(title_bar, text='  ➖  ', font = 'Fredoka-Bold', command = minimize, bg = '#2F3136', fg = "yellow")
-min_button.pack(side=LEFT)
-
-expand_button = Button(title_bar, text='  🗖  ', font = 'Fredoka-Bold', command = maximize, bg = '#2F3136', fg = "green")
+expand_button = Button(title_bar, text='   🗖   ', command = maximize, bg = '#2F3136', fg = "green")
 expand_button.pack(side=LEFT)
 
 root.mainloop()
