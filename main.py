f7f6ff2d6cfe4e4329b2c75a1d44052796977085
Sakiffy <78905237+Berkerserker@users.noreply.github.com>
2022-03-19 23:28:36 +1030

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 root = Tk()
 root.geometry("700x600")
 root.resizable(True, True)
-root.config(bg='#2F3136')
+root.config(bg='#2F3136', cursor = "plus")
 root.overrideredirect(True)
 root.eval('tk::PlaceWindow . center')
 
@@ -30,18 +30,35 @@ def maximize():
         root.maximized = not root.maximized
         # now it is not maximized
 
+def generateCommand():
+    print("Generating...")
+
 title_bar = Frame(root, bg = '#2F3136', relief = 'raised')
 title_bar.pack(side=TOP, fill=BOTH)
 
-title = Label(title_bar, text = 'Random Texturepack Generator     ', bg = '#2F3136', fg = "#F0F0F0")
+title = Label(title_bar, text = 'Random Texturepack Generator', bg = '#2F3136', fg = "#F0F0F0")
 title.pack(side=RIGHT)
 
 title_bar.bind('<B1-Motion>', move)
 
 close_button = Button(title_bar, text='  ❌  ', command = root.destroy, bg = '#2F3136', fg = "red")
-close_button.pack(side=LEFT)
+close_button.pack(side = LEFT)
 
 expand_button = Button(title_bar, text='   🗖   ', command = maximize, bg = '#2F3136', fg = "green")
-expand_button.pack(side=LEFT)
+expand_button.pack(side = LEFT)
+
+root.loadimage1 = tk.PhotoImage(file="button.png")
+root.generateButton = tk.Button(root, image=root.loadimage1, activebackground='#2F3136', cursor = "tcross", command = generateCommand)
+root.generateButton["bg"] = "#2F3136"
+root.generateButton["border"] = "0"
+root.generateButton.pack(side = BOTTOM, pady = 50)
+
+root.loadimage2 = tk.PhotoImage(file="tags.png")
+root.tagsButton = tk.Label(root, image=root.loadimage2, activebackground='#2F3136')
+root.tagsButton["bg"] = "#2F3136"
+root.tagsButton["border"] = "0"
+root.tagsButton.pack(side = TOP, pady = 10)
+
+tagsBox = Text(root, height = 10 , width = 5)
 
 root.mainloop()
