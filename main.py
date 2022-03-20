869c830ae75357a77ea92cd75b10baabfc3f5621
Scnk <73734348+Miendy@users.noreply.github.com>
2022-03-20 14:37:25 +1030

    my coe3e

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from email.mime import image
 import tkinter as tk
 from tkinter import *
 from PIL import ImageTk, Image
@@ -35,19 +34,19 @@ def generateCommand():
 title_bar = Frame(root, bg = '#2F3136', relief = 'raised')
 title_bar.pack(side=TOP, fill=BOTH)
 
-title = Label(title_bar, text = 'Texturepack Idea Generator', bg = '#2F3136', fg = "#F0F0F0")
-title.pack(side=RIGHT)
+title = Label(title_bar, text = 'Random Texturepack Generator   ', bg = '#2F3136', fg = "#F0F0F0")
+title.pack(side = RIGHT)
 
 title_bar.bind('<B1-Motion>', move)
 
-close_button = Button(title_bar, text = "   🔴   " , command = root.destroy, bg = '#2F3136', fg = "red")
+close_button = Button(title_bar, text='  🔴  ', command = root.destroy, bg = '#2F3136', fg = "red")
 close_button.pack(side = LEFT)
 
 expand_button = Button(title_bar, text='   🟢   ', command = maximize, bg = '#2F3136', fg = "green")
 expand_button.pack(side = LEFT)
 
-root.loadimage1 = tk.PhotoImage(file="generate.png")
-root.generateButton = tk.Button(root, image=root.loadimage1, activebackground='#2F3136', cursor = "exchange", command = generateCommand)
+root.loadimage1 = tk.PhotoImage(file="button.png")
+root.generateButton = tk.Button(root, image=root.loadimage1, activebackground='#2F3136', cursor = "tcross", command = generateCommand)
 root.generateButton["bg"] = "#2F3136"
 root.generateButton["border"] = "0"
 root.generateButton.pack(side = BOTTOM, pady = 50)
@@ -58,13 +57,7 @@ root.tagsButton["bg"] = "#2F3136"
 root.tagsButton["border"] = "0"
 root.tagsButton.pack(side = TOP, pady = 10)
 
-root.loadimage3 = tk.PhotoImage(file="generate.png")
-root.version1Button = tk.Button(root, image=root.loadimage3, activebackground='#2F3136', cursor = "exchange", command = generateCommand)
-root.version1Button["bg"] = "#2F3136"
-root.version1Button["border"] = "0"
-root.version1Button.pack(side = TOP, pady = 50)
-
-frame = Frame(root, width = 500, height = 300, borderwidth = 0, highlightthickness = 0, bg = "#2F3136")
+frame = Frame(root, width = 500, height = 300, borderwidth = 0, highlightthickness = 0, background = '#2F3136')
 frame.pack()
 frame.place(anchor = 'center', relx = 0.5, rely = 0.4)
 
@@ -72,5 +65,12 @@ txtWallpaper = ImageTk.PhotoImage(Image.open("text.png"))
 
 imageLabel = Label(frame, image = txtWallpaper, bd = 0)
 imageLabel.pack()
+
+menu = StringVar()
+menu.set("Select Vertion")
+
+dropDown = OptionMenu(root, menu, "1.8", "1.16", "1.17", "1.18")
+dropDown.config(bg = "#2F3136", activebackground = "#2F3136", highlightthickness = 0)
+dropDown.pack(side = BOTTOM)
 
 root.mainloop()
